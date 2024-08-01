5d9e413342dff2fdd7daf532c40151c627acee9b
lusate <rayrayr@naver.com>
2024-08-01 16:14:50 +0900

    Feat: SideBar 생성 /#1
    부족한 부분이 있지만 일단 사이드바 생성 완료. 나중에 Link를 추가해야 함.
    이제 스캔 시작 등 버튼 생성을 해야 함.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 export default function Home() {
   return <main>첫 페이지 로그인</main>
 }
--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -1,3 +1,18 @@
+'use client'
+import Header from '@/app/components/sidebar/header'
+import SideBar from '@/app/components/sidebar/sidebar'
+
 export default function Scan() {
-  return <main>scan 페이지</main>
+  return (
+    <main className='lt-4 flex h-full min-h-screen w-full flex-col items-center justify-start p-3'>
+      스캔 페이지
+      <div className='App'>
+        {/* <Header /> */}
+
+        <div className='container'>
+          <SideBar />
+        </div>
+      </div>
+    </main>
+  )
 }
