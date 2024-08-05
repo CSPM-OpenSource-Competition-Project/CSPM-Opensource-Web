4e6bd0a3c5e7195033ee8fe1643736097139a374
lusate <rayrayr@naver.com>
2024-08-05 10:46:15 +0900

    dev pull

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,12 @@
-import MainLogin from '@/app/components/loginComponents/mainLogin'
-import Container from '@/app/utils/container'
+import MainLogin from '@/components/loginComponents/mainLogin'
+import Container from '@/utils/container'
 
 export default function Login() {
   return (
     <main className='flex h-screen w-full items-center justify-center'>
-      <MainLogin />
+      <Container>
+        <MainLogin />
+      </Container>
     </main>
   )
 }
