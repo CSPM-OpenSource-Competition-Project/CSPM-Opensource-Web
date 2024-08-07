834cae8d6b9709dbae6364efaebeba6a178f068c
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2024-08-07 17:36:25 +0900

    merge

--- src/app/compliance/page.tsx
+++ src/app/compliance/page.tsx
@@ -1,4 +1,9 @@
+import ComplianceMultiFilterMenuBar from '../../components/complianceField/ComplianceMultiFilterMenuBar'
 export default function Compliance() {
-  return <main className='flex h-full w-full flex-col items-center justify-between p-3'>compliance 페이지</main>
+  return( 
+  <main className='flex h-full w-full flex-col items-center justify-between p-3'>
+    <ComplianceMultiFilterMenuBar/>
+  </main>
+  )
 }
 
---
+++ src/components/complianceField/ComplianceMultiFilterMenuBar.tsx
@@ -0,0 +1,7 @@
+export default function MultiFilterMenuBar() {
+    return(
+        <div>
+            complianceMultifilter
+        </div>
+    )
+}
