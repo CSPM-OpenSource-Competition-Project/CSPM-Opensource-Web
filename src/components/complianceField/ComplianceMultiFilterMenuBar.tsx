cf67ed531ea2db30a8b9e3f326c3197561cfb266
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2024-08-08 15:18:41 +0900

    feat: 취약점 타이틀 선택 박스/#4

--- src/components/complianceField/ComplianceMultiFilterMenuBar.tsx
+++ src/components/complianceField/ComplianceMultiFilterMenuBar.tsx
@@ -1,9 +1,11 @@
 import VulnerabilityLevel from '../../components/select/vulnerabilityLevel'
+import VulnerabilityTitle from '../../components/select/vulnerabilityTitle'
 
 export default function MultiFilterMenuBar() {
-    return(
-        <div>
-           <VulnerabilityLevel/>
-        </div>
-    )
+  return (
+    <div>
+      <VulnerabilityLevel />
+      <VulnerabilityTitle />
+    </div>
+  )
 }
