07eb224c4b02ba982ed955c43175bd1c824b0f88
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2024-08-16 10:46:32 +0900

    feat: 취약점 스캔 table /#4

--- src/app/compliance/page.tsx
+++ src/app/compliance/page.tsx
@@ -1,8 +1,14 @@
 import ComplianceMultiFilterMenuBar from '../../components/complianceField/ComplianceMultiFilterMenuBar'
+import ComplianceTableComponent from '../../components/complianceField/ComplianceTableComponent'
 export default function Compliance() {
   return (
-    <main className=''>
-      <ComplianceMultiFilterMenuBar />
+    <main className='grid grid-cols-1 gap-4'>
+      <div>
+        <ComplianceMultiFilterMenuBar />
+      </div>
+      <div className='mt-10'>
+        <ComplianceTableComponent />
+      </div>
     </main>
   )
 }
