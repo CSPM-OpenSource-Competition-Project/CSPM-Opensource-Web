5e1aded7dbb801e1de2b01f6a3e0cf33178a9eb1
lusate <rayrayr@naver.com>
2024-08-21 18:54:03 +0900

    feat: create fetch  scanStart button

--- src/components/button/startScanButton.tsx
+++ src/components/button/startScanButton.tsx
@@ -5,15 +5,8 @@ import ButtonLayer from '@/components/button/buttonLayer'
 ;('@/stores/selectStore')
 import { useSelectType } from '@/stores/selectStore'
 
-const BASE_URL = process.env.NEXT_PUBLIC_NEXT_APP_BASE_URL
-
 export default function StartScanButton() {
-  const { setIAMSelected, setGroupSelected } = useSelectType()
-
-  // const handleStartScan = () => {
-  //   setIAMSelected
-  //   setGroupSelected
-  // }
+  const { iAMSelected, groupSelected } = useSelectType()
 
   const [dataResult, setDataResult] = useState('')
 
@@ -21,42 +14,42 @@ export default function StartScanButton() {
    * 스캔 시작 버튼 기능 진행 중
    * 필터 2개 선택해야 스캔 시작이 가능하도록 처리
    */
-
-  // const scanList = scanClient.map((client, index) => ({
-  //   client,
-  //   accountId: scanAccountIdList[index],
-  //   accountName: scanAccountName[index],
-  // }))
+  const scanList = {
+    iAMSelected,
+    groupSelected,
+  }
 
   const scanStart = async () => {
-    //   try {
-    //     const response = await fetch(`${BASE_URL}/resources`, {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json; charset=utf-8',
-    //       },
-    //       body: JSON.stringify(scanList),
-    //     })
-    //     if (response.ok) {
-    //       const inner = await response.json()
-    //       const data = inner
-    //       setDataResult(data.message)
-    //     }
-    //   } catch (error) {
-    //     setDataResult('api 연결이 안됩니다.')
-    //   }
+    try {
+      const response = await fetch('resources', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(scanList),
+      })
+      if (response.ok) {
+        const inner = await response.json()
+        const data = inner
+        setDataResult(data.message)
+      }
+    } catch (error) {
+      setDataResult('api 연결이 안됩니다.')
+    }
   }
 
-  const handleScanStart = () => {
-    scanStart()
-  }
+  // const handleScanStart = () => {
+  //   scanStart()
+  // }
 
-  // useEffect(() => {}, [dataResult])
+  useEffect(() => {
+    console.log('ResourceResult가 스캔되었습니다.', dataResult)
+  }, [dataResult])
 
   return (
     // 스캔 시작 버튼
     <div className='mt-8'>
-      <ButtonLayer buttonStyle='bg-[#002865]' childText='스캔 시작' method={handleScanStart} />
+      <ButtonLayer buttonStyle='bg-[#002865]' childText='스캔 시작' method={scanStart} />
     </div>
   )
 }
