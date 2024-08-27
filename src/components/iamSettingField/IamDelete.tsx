6213fb73226801ed91800d3a7179853c00c25aef
luda412 <201810791@sangmyung.kr>
2024-08-27 13:37:44 +0900

    feat: IAM 삭제 구현 /#32

--- src/components/iamSettingField/IamDelete.tsx
+++ src/components/iamSettingField/IamDelete.tsx
@@ -1,23 +1,31 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import IAMSelectBox from '@/components/select/iAMSelectBox'
 import apiFetch from '@/utils/fetchWrapper'
 
 export default function IamDelete() {
-  const handleSubmit = async () => {
+  const [nickname, setnickname] = useState('')
+
+  const handleDelete = async (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+
     try {
-      const resourceId = 'your-resource-id' // 실제 삭제할 리소스의 ID를 여기에 설정
-      const [statusCode, data] = await apiFetch(`/api/resource/${resourceId}`, {
+      const [statusCode, data] = await apiFetch(`/api/iamsettings`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
         },
+        body: JSON.stringify({
+          nickname: nickname,
+        }),
       })
       if (statusCode === 200) {
         console.log('삭제 성공:', data)
+        alert('IAM 삭제 성공')
       } else {
         console.log('삭제 실패:', statusCode)
+        alert('IAM 삭제 실패')
       }
     } catch (e) {
       console.error('삭제 중 오류 발생:', e)
@@ -28,7 +36,7 @@ export default function IamDelete() {
     <div className='ml-15 flex flex-row justify-center gap-4'>
       <IAMSelectBox />
       <button
-        onClick={handleSubmit}
+        onClick={handleDelete}
         type='button' // 이 부분을 수정
         className='mt-7 h-12 w-20 rounded-lg bg-blue-500 text-lg font-bold text-white'
       >
